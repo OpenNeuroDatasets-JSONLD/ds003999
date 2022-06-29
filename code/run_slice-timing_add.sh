d2a923f33c9e22a90f018f86d8e432174826726f
Dmitriy Mikhailov <dmitriy_mikhailov@ispras.ru>
2022-06-29 10:03:53 +0300

    Remove unnesesary code

--- code/run_slice-timing_add.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-bids-validator ./ --json --verbose | \
-    jq '.issues.warnings[0].files[].file.path' | \
-    sed 's/nii\.gz\"/json/; s/^\"//' | \
-    xargs code/slice-timing_add.py
